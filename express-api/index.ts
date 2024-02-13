191709cbc92856997fd85611572316081eb80636
Brian Valdivieso <brian.valdivieso.dev@gmail.com>
2024-02-13 11:18:31 +0100

    Add rabbitMq and sockets

--- express-api/index.ts
+++ express-api/index.ts
@@ -1,16 +1,100 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import amqp from 'amqplib';
+import { Server } from 'socket.io';
+import http from 'http';
 
-const app = express();
+
+
 const port = process.env.PORT || 4000;
 const app_name = process.env.APP_NAME || 'express-api';
+let channel:amqp.Channel | null = null;
+const queue = 'test_queue';
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());
+
+
+const connectRabbit = async () => {
+    const rabbitConnection = await amqp.connect(process.env.RABBITMQ_URI || 'amqp://brian:123456@localhost:5672');
+    channel = await rabbitConnection.createChannel();
+    await channel.assertQueue(queue, { durable: false });
+    if(channel === null){
+        console.log('No channel created');
+        return;
+    }
+    channel.consume(queue, (msg) => {
+        if(msg === null){
+            console.log('No message in queue');
+            return;
+        }
+        console.log(`Received message: ${msg.content.toString()}`);
+    });
+}
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('message', (msg) => {
+        console.log(`Received message: ${msg}`);
+        if(msg.includes("Hello")){
+            socket.emit('notification', `Hello back, I am ${app_name}`);
+        }
+    });
+});
+
+
 app.use('/whoareyou', (req, res)=>{
     res.status(200);
     res.send(`I am ${app_name}`);
 });
 
-app.listen(port, () => console.log(`${app_name} listening on port ${port}`));
+app.post('/send', (req, res) => {
+    const message = req.body.message;
+    if(!message){
+        res.status(400);
+        res.send('No message provided');
+        return;
+    }
+    if(!channel){
+        res.status(500);
+        res.send('No channel available');
+        return;
+    }
+    console.log(`Sending message: ${message}`);
+    channel.sendToQueue(queue, Buffer.from(message));
+    res.status(200);
+    res.send('Message sent');
+});
+
+app.post('/notification', (req, res) => {
+    const message = req.body.message;
+    if(!message){
+        res.status(400);
+        res.send('No message provided');
+        return;
+    }
+    console.log(`Sending notification: ${message}`);
+    io.emit('notification', message);
+    res.status(200);
+    res.send('Message sent');
+});
+
+connectRabbit();
+
+server.listen(port, () => console.log(`${app_name} listening on port ${port}`));
--- react-front/src/App.tsx
+++ react-front/src/App.tsx
@@ -1,28 +1,70 @@
-import axios from "axios"
-import { useEffect } from "react"
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 function App() {
+  const socketUrl = import.meta.env.VITE_SOCKETIO_URL;
+  const socket = io(socketUrl);
 
-  const makeRequest = async (url:string) => {
-    console.log("Who are you?")
-    const res = await axios.get(url)
-    console.log(res.data)
-  }
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [notifications, setNotifications] = useState<string[]>([]);
+
+  const makeRequest = async (url: string) => {
+    console.log('Who are you?');
+    const res = await axios.get(url);
+    console.log(res.data);
+  };
+
+  const sayHello = () => {
+    console.log('Hello');
+    socket.emit('message', `Hello i am ${import.meta.env.VITE_APP_NAME}`);
+  };
+
   useEffect(() => {
-      return () => {
-      console.log(`I am ${import.meta.env.VITE_APP_NAME}`)
-      if(import.meta.env.VITE_API_URL) {
-        makeRequest(import.meta.env.VITE_API_URL)
+    return () => {
+      console.log(`I am ${import.meta.env.VITE_APP_NAME}`);
+      if (import.meta.env.VITE_API_URL) {
+        makeRequest(import.meta.env.VITE_API_URL);
       }
-    }
-  },[])  
+
+      socket.on('connect', () => {
+        setIsConnected(true);
+        sayHello();
+      });
+      socket.on('disconnect', () => {
+        setIsConnected(false);
+      });
+
+      socket.on('notification', (data: string) => {
+        notifications.push(data);
+        setNotifications([...notifications]);
+      });
+
+      socket.on('connect_error', (error: Error) => {
+        console.error('Connection error', error);
+      });
+      socket.on('connect_timeout', (timeout: number) => {
+        console.error('Connection timeout', timeout);
+      });
+    };
+  }, []);
   return (
     <>
       <h1>Micro Frontend with React and Vite</h1>
       <h2>APP NAME: {import.meta.env.VITE_APP_NAME}</h2>
       <h2>APP PORT: {import.meta.env.VITE_APP_PORT}</h2>
+
+      <h2>
+        SOCKETS: {isConnected ? 'Connected' : `Connection to ${socketUrl} ...`}
+      </h2>
+      <button onClick={sayHello}>Say hello</button>
+      <h3>Notifications</h3>
+      <ul>
+        {notifications.map((notification, index) => (
+          <li key={index}>{notification}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
